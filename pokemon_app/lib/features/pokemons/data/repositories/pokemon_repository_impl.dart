0e70323b64b54527a30c9a84179936183ae30905
zd-prog <darya.ziziko@mail.ru>
2023-01-17 21:19:09 +0300

    [Realization] Repository methods implementation

--- pokemon_app/lib/features/pokemons/data/repositories/pokemon_repository_impl.dart
+++ pokemon_app/lib/features/pokemons/data/repositories/pokemon_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:pokemon_app/core/error/exception.dart';
 import 'package:pokemon_app/core/error/failure.dart';
 import 'package:pokemon_app/core/platform/network_info.dart';
 import 'package:pokemon_app/features/pokemons/data/datasources/pokemon_local_datasource.dart';
@@ -19,14 +20,48 @@ class PokemonRepositoryImpl extends PokemonRepository {
   );
 
   @override
-  Future<Either<Failure, PokemonDetails>> getPokemonDetails(String url) {
-    // TODO: implement getPokemonDetails
-    throw UnimplementedError();
+  Future<Either<Failure, PokemonDetails>> getPokemonDetails(String url) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePokemonDetails =
+            await remoteDatasource.getPokemonDetails(url);
+        localDatasource.cachePokemonDetails(remotePokemonDetails);
+        return Right(remotePokemonDetails);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localPokemonDetails =
+            await localDatasource.getPokemonDetails(url);
+        return Right(localPokemonDetails);
+      } on CacheException {
+        return Left(
+          CacheFailure(),
+        );
+      }
+    }
   }
 
   @override
-  Future<Either<Failure, List<Pokemon>>> getPokemons() {
-    // TODO: implement getPokemons
-    throw UnimplementedError();
+  Future<Either<Failure, List<Pokemon>>> getPokemons() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePokemons = await remoteDatasource.getPokemons();
+        localDatasource.cachePokemons(remotePokemons);
+        return Right(remotePokemons);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localPokemons = await localDatasource.getPokemons();
+        return Right(localPokemons);
+      } on CacheException {
+        return Left(
+          CacheFailure(),
+        );
+      }
+    }
   }
 }
