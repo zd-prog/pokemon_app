1c6a5cea3d0ed0ad4081f34fcfa240116b94a4b7
zd-prog <darya.ziziko@mail.ru>
2023-01-17 22:33:46 +0300

    [Realization] GetPokemons method implementation

--- pokemon_app/lib/features/pokemons/data/datasources/pokemon_remote_datasource.dart
+++ pokemon_app/lib/features/pokemons/data/datasources/pokemon_remote_datasource.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pokemon_app/core/error/exception.dart';
 import 'package:pokemon_app/features/pokemons/data/models/pokemon_details_model.dart';
 import 'package:pokemon_app/features/pokemons/data/models/pokemon_model.dart';
 
@@ -5,4 +9,38 @@ abstract class PokemonRemoteDatasource {
   Future<List<PokemonModel>> getPokemons();
 
   Future<PokemonDetailsModel> getPokemonDetails(String url);
+}
+
+class PokemonRemoteDatasourceImpl implements PokemonRemoteDatasource {
+  final http.Client client;
+
+  PokemonRemoteDatasourceImpl(this.client);
+
+  @override
+  Future<PokemonDetailsModel> getPokemonDetails(String url) {
+    // TODO: implement getPokemonDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<PokemonModel>> getPokemons() async {
+    final response = await client.get(
+      Uri.parse(
+        'https://pokeapi.co/api/v2/pokemon',
+      ),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var pokemons = json
+          .decode(response.body)['results']
+          .map<PokemonModel>((data) => PokemonModel.fromJson(data))
+          .toList();
+      return pokemons;
+    } else {
+      throw ServerException();
+    }
+  }
 }
